9dad50b31527e014223008cd1ef76270549fc5e4
Bob Ying <124599792@qq.com>
2018-05-30 14:18:42 +0800

    添加证书和友盟share

--- app/src/main/java/com/grjf365/gongrongpoints/Application.java
+++ app/src/main/java/com/grjf365/gongrongpoints/Application.java
@@ -7,9 +7,11 @@ import com.orhanobut.logger.AndroidLogAdapter;
 import com.orhanobut.logger.FormatStrategy;
 import com.orhanobut.logger.Logger;
 import com.orhanobut.logger.PrettyFormatStrategy;
+import com.umeng.socialize.PlatformConfig;
+import com.umeng.commonsdk.UMConfigure;
 
 import cn.jiguang.share.android.api.JShareInterface;
-import cn.jiguang.share.android.api.PlatformConfig;
+//import cn.jiguang.share.android.api.PlatformConfig;
 import cn.jpush.android.api.JPushInterface;
 
 /**
@@ -17,14 +19,33 @@ import cn.jpush.android.api.JPushInterface;
  */
 
 public class Application extends android.app.Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // 友盟初始化
+        UMConfigure.init(this, "5b0e2b608f4a9d435c0001c9"
+                , "umeng", UMConfigure.DEVICE_TYPE_PHONE, "");//58edcfeb310c93091c000be2 5965ee00734be40b580001a0
+
+        PlatformConfig.setWeixin("wxf177c6755716fa32", "8ced9a149b6c5b79e2bcb682092256e2");
+        //豆瓣RENREN平台目前只能在服务器端配置
+        PlatformConfig.setSinaWeibo("3921700954", "04b48b094faeb16683c32669824ebdad","http://sns.whalecloud.com");
+        PlatformConfig.setQQZone("100424468", "c7394704798a158208a74ab60104f0ba");
+        PlatformConfig.setAlipay("2015111700822536");
+
+    }
+
     @Override
     protected void attachBaseContext(Context base) {
         super.attachBaseContext(base);
         MultiDex.install(this);
+
+
         JPushInterface.setDebugMode(true); 	// 设置开启日志,发布时请关闭日志
 //        JPushInterface.init(this);     		// 初始化 JPush
-        PlatformConfig platformConfig = new PlatformConfig()
-                .setWechat("wxf177c6755716fa32", "dcad950cd0633a27e353477c4ec12e7a");
+//        PlatformConfig platformConfig = new PlatformConfig()
+//                .setWechat("wxf177c6755716fa32", "dcad950cd0633a27e353477c4ec12e7a");
         /**
          * since 1.5.0，1.5.0版本后增加API，支持在代码中设置第三方appKey等信息，当PlatformConfig为null时，或者使用JShareInterface.init(Context)时需要配置assets目录下的JGShareSDK.xml
          **/
